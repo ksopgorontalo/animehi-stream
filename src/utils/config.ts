33d4d38766cd44aee2c17285e5fa1a8d8044f6e1
ksopgorontalo <49975979+ksopgorontalo@users.noreply.github.com>
2023-03-17 17:29:49 +0800

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,7 +1,7 @@
 export const BASE_URL = 'https://api.consumet.org';
 export const PAGE = 1;
 export const CORS_PROXY = 'https://api.consumet.org/';
-export const GOGO_PROVIDER = 'gogoanime';
+export const GOGO_PROVIDER = 'gogocdn';
 export const ZORO_PROVIDER = 'zoro';
 export const TYPE = {
   ANIME: 'ANIME',
